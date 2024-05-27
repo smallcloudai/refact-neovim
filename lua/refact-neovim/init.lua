b07ee5f850c9205510e48a54cafcb5b6d432c81e
Marco Meijer <marcomeijerm@gmail.com>
2024-05-27 11:35:13 +0200

    fix: lsp not starting up when installing plugin using packer

--- lua/refact-neovim/init.lua
+++ lua/refact-neovim/init.lua
@@ -1,27 +1,29 @@
 local refact_lsp = require("refact-neovim.lsp")
-local config_module = require("refact-neovim.config")
+local config = require("refact-neovim.config")
 local completion = require("refact-neovim.completion")
 local api = vim.api
 
 local function setup(user_config)
-  config_module.setup(user_config)
-  local config = config_module.get()
+  config.setup(user_config)
 
-  refact_lsp.setup()
+  -- todo: figure out why the lsp is not able to startup correctly without this timer
+  vim.fn.timer_start(0, function()
+    refact_lsp.setup()
 
-  api.nvim_create_autocmd("InsertLeave", {
-    callback = completion.cancel
-  })
+    api.nvim_create_autocmd("InsertLeave", {
+      callback = completion.cancel
+    })
 
-  api.nvim_create_autocmd("InsertEnter", {
-    callback = completion.schedule
-  })
+    api.nvim_create_autocmd("InsertEnter", {
+      callback = completion.schedule
+    })
 
-  api.nvim_create_autocmd("CursorMovedI", {
-    callback = completion.schedule
-  })
+    api.nvim_create_autocmd("CursorMovedI", {
+      callback = completion.schedule
+    })
 
-  vim.keymap.set("i", config.accept_keymap, completion.accept_suggestion, { expr = true })
+    vim.keymap.set("i", config.get().accept_keymap, completion.accept_suggestion, { expr = true })
+  end)
 end
 
 return {
